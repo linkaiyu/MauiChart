21a2e1ac89489bf98bc6c39f638433ebe7443b71
linkaiyu <lyu11111@hotmail.com>
2023-03-16 20:45:50 -0400

    add property to chartdrawable

--- MauiChart/ChartDrawable.cs
+++ MauiChart/ChartDrawable.cs
@@ -4,35 +4,21 @@ namespace MauiChart.Drawables
 {
 	internal class ChartDrawable : IDrawable
 	{
+		public Line[] lines { get; set; }
+
 		public void Draw(ICanvas canvas, RectF dirtyRect)
 		{
-			int lineCount = 5;
-			Line[] lines = new Line[lineCount];
+			if (lines == null)
+				return;
 
 
-			for (int i = 0; i < lineCount; i++)
+			for (int i = 0; i < lines.Length; i++)
 			{
-				lines[i] = new Line() { start = new PointF(i * 40, 0), end = new PointF((i + 1) * 40, 0) };
+				if (lines[i].start.Y > lines[i].end.Y)
+					canvas.StrokeColor = Colors.Green;
+				else
+					canvas.StrokeColor = Colors.Red;
 
-			}
-
-			lines[0].start.Y = 0;
-			lines[0].end.Y = 30;
-
-			lines[1].start.Y = 30;
-			lines[1].end.Y = 20;
-
-			lines[2].start.Y = 20;
-			lines[2].end.Y = 60;
-
-			lines[3].start.Y = 60;
-			lines[3].end.Y = 20;
-
-			lines[4].start.Y = 20;
-			lines[4].end.Y = 70;
-
-			for (int i = 0; i < lineCount; i++)
-			{
 				canvas.DrawLine(lines[i].start, lines[i].end);
 
 			}
@@ -40,7 +26,7 @@ namespace MauiChart.Drawables
 
 	}
 
-	internal class Line
+	public class Line
 	{
 		public PointF start;
 		public PointF end;
--- MauiChart/MainPage.xaml.cs
+++ MauiChart/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Maui;
+﻿using MauiChart.Drawables;
+using Microsoft.Maui;
 using Microsoft.Maui.Controls;
 
 namespace MauiChart;
@@ -26,7 +27,39 @@ public partial class MainPage : ContentPage
 		//	"10:20", "223",
 		//	}
 
-		graphicsView.Invalidate();
+		// for now, we just pass the line info 
+		var chartDrawable = graphicsView.Drawable as ChartDrawable; 
+		if (chartDrawable != null)
+		{
+			int lineCount = 5;
+			Line[] lines = new Line[lineCount];
+
+
+			for (int i = 0; i < lineCount; i++)
+			{
+				lines[i] = new Line() { start = new PointF(i * 40, 0), end = new PointF((i + 1) * 40, 0) };
+
+			}
+
+			lines[0].start.Y = 0;
+			lines[0].end.Y = 30;
+
+			lines[1].start.Y = 30;
+			lines[1].end.Y = 20;
+
+			lines[2].start.Y = 20;
+			lines[2].end.Y = 60;
+
+			lines[3].start.Y = 60;
+			lines[3].end.Y = 20;
+
+			lines[4].start.Y = 20;
+			lines[4].end.Y = 70;
+
+			chartDrawable.lines = lines;
+
+			graphicsView.Invalidate();
+		}
 
 	}
 }
